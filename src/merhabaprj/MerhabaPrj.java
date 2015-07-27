609a8882b6ddb4313898590fa941a9ec538bfa53
Administrator <Administrator@192.168.2.23>
2015-07-27 11:56:43 +0300

    Merhaba Prj ilk versiyonb

---
+++ src/merhabaprj/MerhabaPrj.java
@@ -0,0 +1,14 @@
+
+package merhabaprj;
+
+
+public class MerhabaPrj {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Merhaba Murat");
+    }
+    
+}
